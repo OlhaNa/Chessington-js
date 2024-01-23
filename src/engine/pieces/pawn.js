d6e37df38d30aa37b8be56d84393457b71297f3e
OlhaNa <olha.nadvodna@gmail.com>
2024-01-23 16:09:33 +0000

    finished Red-2, Red-3

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -1,6 +1,8 @@
 import Player from '../player';
 import Square from '../square';
 import Piece from './piece';
+import Board from '../board';
+
 
 export default class Pawn extends Piece {
     constructor(player) {
@@ -8,11 +10,30 @@ export default class Pawn extends Piece {
     }
 
     getAvailableMoves(board) {
-        let location = board.findPiece(this)
+        let location = board.findPiece(this);
+        const options = [location.row + 1, location.row + 2];
+        const blackOptions = [Square.at(location.row - 1, location.col), Square.at(location.row - 2, location.col)];
         if (this.player === Player.WHITE) {
-            return Square.at(location.row + 1, location.col)
+            let blackLocation = board.findPiece(Player.BLACK);
+            if (Square.at(location.row + 1, location.col) ) {
+                return [];
+            }
+            if (location.row >= 2 && location.row < 7) {
+                return [Square.at(location.row + 1, location.col)];
+            } else if (location.row === 1) {
+                return [Square.at(options[0], location.col), Square.at(options[1], location.col)];
+            }
         } else {
-            return Square.at(location.row - 1, location.col)
+            if (location.row < 6 && location.row > 0) {
+                return [Square.at(location.row - 1, location.col)];
+                
+            } else if (location.row === 6) {
+                return [blackOptions[0], blackOptions[1]];
+            }
         }
     }
-}
+
+ }
+
+
+            
--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -1,4 +1,9 @@
 import Piece from './piece';
+import Board from '../board';
+import Square from '../square';
+import Player from '../player';
+
+
 
 export default class Rook extends Piece {
     constructor(player) {
@@ -6,6 +11,25 @@ export default class Rook extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let location = board.findPiece(this);
+        let moves = [];
+    
+        // Check all squares in the rook's row
+        for (let i = 0; i < 8; i++) {
+            if (i !== location.col) {
+                moves.push(Square.at(location.row, i));
+            }
+        }
+    
+        // Check all squares in the rook's column
+        for (let i = 0; i < 8; i++) {
+            if (i !== location.row) {
+                moves.push(Square.at(i, location.col));
+            }
+        }
+    
+        return moves;
     }
 }
+
+
